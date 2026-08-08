0345b49edde131eae788a65147cbd3c39494bdae
anshu0089r <anshuraj38629@gmail.com>
2026-08-09 00:05:26 +0530

    learn about post

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 from fastapi import FastAPI ,Path, HTTPException,Query
+from fastapi.responses import JSONResponse
 import json
+from pydantic import BaseModel ,Field,computed_field
+from typing import Annotated,Literal
 
 app= FastAPI()
+
+class Patient(BaseModel):
+    id: Annotated[str,Field(...,description='Id of the patient',examples=['P001'])]
+    name: Annotated[str,Field(...,description='Name of the patient')]
+    city:Annotated[str,Field(...,description='City of the patient where he is living ')]
+    age:Annotated[int,Field(...,gt=0,lt=100,description="Age of the patient ")]
+    gender:Annotated[Literal['male','female','others'],Field(...,description='Gender of the patient ')]
+    height:Annotated[float,Field(...,gt=0,description='Height of the patient in mtrs')]
+    weight:Annotated[float,Field(...,gt=0,description='weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi= round(self.weight/(self.height**2),2)
+        return bmi
+
+    @computed_field
+    @property
+    def verdict(self) ->str:
+        if self.bmi <18.5:
+            return 'underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi <30:
+            return 'Over-weight'
+        else:
+            return "obese"
+
+        
+
 
 def load_data():
     with open('patients.json','r') as f:
         data = json.load(f)
     return data
+
+def save_data(data):
+    with open('patients.json','w')as f:
+        json.dump(data,f)
 
 
 @app.get("/")
@@ -32,7 +69,7 @@ def view_patient(patient_id:str = Path(..., description = 'Id of the patient in 
         return data[patient_id]
     return HTTPException(status_code=400,detail='Patient not found')
 
-@app.get('sort')
+@app.get('/sort')
 def sort_patients(sort_by:str=Query(...,description='sort on the basis of height ,weight or bmi'),order:str= Query('asc',description = 'sort in asc or desc order ')):
    
     valid_fields= ['height','weight','bmi']
@@ -50,3 +87,19 @@ def sort_patients(sort_by:str=Query(...,description='sort on the basis of height
     sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
 
     return sorted_data
+
+@app.post('/create')
+def create_patient(patient:Patient):
+
+    #load the existing data 
+    data= load_data()
+    #check if the patient is already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400,detail='Patient already registred')
+    #new patient added to the database
+    data[patient.id]=patient.model_dump(exclude=['id'])
+
+    # save in the json file
+    save_data(data)
+
+    return JSONResponse(status_code=201,content={'message':'patient created sucessfully'})
